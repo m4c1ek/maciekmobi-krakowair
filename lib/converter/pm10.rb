ef7e9e267a52db0682627d2868dcb4e63447ed6d
Maciej Walczynski <maciej.walczynski@schibsted.pl>
2013-08-05 13:51:09 +0200

    parsing rows

--- lib/converter/pm10.rb
+++ lib/converter/pm10.rb
@@ -1,11 +1,18 @@
 require 'nokogiri'
 require 'json'
 
+
+class String
+  def is_integer?
+    self.to_i.to_s == self
+  end
+end
+
 module Converter
 	class PM10
 		def convert(html)
 			page = Nokogiri::HTML(html)
-			timestamps = page.css('body table tbody')[1]
+			timestamps = page.css('body table tbody tr')
 
 			data = { :data => [
 				{ :timestamp => 12, :value => 50, :location => "Krakow - Krowodza", :unit => "µg/m" }, 
@@ -17,8 +24,53 @@ module Converter
 
 		def dateFromHtml(html)
 			timeComponents = html.scan(/(\d{4})\-(\d{2})\-(\d{2})/)
-			return (Time.now.to_i - (Time.now.to_i % (60*60*24)) + 60*60*2) if timeComponents.empty? 
-			return Time.gm(timeComponents[0][0], timeComponents[0][1], timeComponents[0][2]).to_i + 60*60*2
+			return (Time.now.to_i - (Time.now.to_i % (60*60*24)) - 60*60*2) if timeComponents.empty? 
+			return Time.gm(timeComponents[0][0], timeComponents[0][1], timeComponents[0][2]).to_i - 60*60*2
 		end
+
+		def dataFromHtml(html)
+			page = Nokogiri::HTML(html)
+			rows = page.css('body table tbody tr')
+			
+		end
+
+		def dataRow?(row)
+			return false if timeRow?(row) 
+			numbers = row.css('td').map {|td| td.text.strip}.select {|text| text.is_integer? }
+			return numbers.size > 0
+		end
+
+		def timeRow?(row)
+			hours = (1..24).to_a
+			values = row.css('td').map {|td| td.text.strip}.select {|text| text.is_integer? }.map {|text| text.to_i}
+			onlyHours = (hours - (hours & values)).length == 0
+			return onlyHours
+		end
+
+		def dataRowValues(row, timeRow, date)
+			rowValues = rowValues(row)
+			timeRowValues = rowValues(timeRow)
+			location = rowLocation(row)
+			combinedValues = []
+			timeRowValues.each_with_index { |value, i|
+				if (!value.nil? && !rowValues[i].nil?) 
+					then combinedValues << {:timestamp => date+(value*60*60), :value => rowValues[i], :unit => "µg/m3" } 
+				end 
+			}
+			return {:location => location, :data => combinedValues }
+		end
+
+		def rowValues(row)
+			values = row.css('td').map { |td| 
+				value = td.text.strip
+				(value.empty? || !value.is_integer?) ? nil : value.to_i
+			}
+			return values
+		end
+
+		def rowLocation(row)
+			row.css('td')[0].text.strip
+		end
+
 	end
 end
--- spec/pm10_spec.rb
+++ spec/pm10_spec.rb
@@ -19,13 +19,41 @@ describe Converter::PM10 do
 	it "should parse date" do
 		contents = File.open("./html/pm10.html", "rb").read
 		date = Converter::PM10.new.dateFromHtml(contents)
-		date.should == 1375149600
+		date.should == 1375135200
 	end
 
 	it "should give today (0:00 GMT+2) on no date string" do
 		date = Converter::PM10.new.dateFromHtml("<html>no date in this html</html>")
-		expectedTime = Time.now.to_i - (Time.now.to_i % (60*60*24)) + 60*60*2
+		expectedTime = Time.now.to_i - (Time.now.to_i % (60*60*24)) - 60*60*2
 		date.should == expectedTime
 	end
-end
+	
+	it "should detect data row" do		
+		page = Nokogiri::HTML(File.open("./html/pm10.html", "rb").read)
+		rows = page.css('body table tr')
+		Converter::PM10.new.dataRow?(rows[0]).should == false
+		Converter::PM10.new.dataRow?(rows[1]).should == false
+		Converter::PM10.new.dataRow?(rows[2]).should == true
+	end
 
+	it "should get data row values" do
+		contents = File.open("./html/pm10.html", "rb").read
+		page = Nokogiri::HTML(contents)
+		rows = page.css('body table tr')
+		row = rows[2]
+		timeRow = rows[1]
+		date = Converter::PM10.new.dateFromHtml(contents)
+		Converter::PM10.new.dataRowValues(row, timeRow, date).should == 
+		{   :location => "Tarnów", 
+			:data => [{:timestamp=>1375138800, :value=>34, :unit=>"µg/m3"},
+         {:timestamp=>1375142400, :value=>35, :unit=>"µg/m3"},
+         {:timestamp=>1375146000, :value=>37, :unit=>"µg/m3"},
+         {:timestamp=>1375149600, :value=>50, :unit=>"µg/m3"},
+         {:timestamp=>1375153200, :value=>64, :unit=>"µg/m3"},
+         {:timestamp=>1375156800, :value=>27, :unit=>"µg/m3"},
+         {:timestamp=>1375160400, :value=>24, :unit=>"µg/m3"},
+         {:timestamp=>1375164000, :value=>21, :unit=>"µg/m3"},
+         {:timestamp=>1375167600, :value=>18, :unit=>"µg/m3"},
+         {:timestamp=>1375171200, :value=>19, :unit=>"µg/m3"}] }
+	end
+end
